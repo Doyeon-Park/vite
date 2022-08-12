2a37974189dedca2c0f2891aa57a0f5f5bbf913c
Doyeon Park (leah) <leah.park@momenti.tv>
2022-08-12 15:55:54 +0900

    Add implement cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <div className="content"></div>
+      <div className="content">
+        <Card title="Text">Hello, world!</Card>
+        <Card title="Card within the card">
+          <p>This is a card within a card.</p>
+          <Card title="Inner card">
+            <p>This is an inner card.</p>
+          </Card>
+        </Card>
+        <Card title="Including image">
+          <img
+            src="https://loremflickr.com/1234/2345/cats?56789"
+            alt="Random cat"
+          />
+        </Card>
+      </div>
     </div>
   );
 }
